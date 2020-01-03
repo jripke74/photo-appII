e399ced4d29e32aaf8036f06d9ef09b781c06d55
jripke74@gmail.com <jripke74@gmail.com>
2020-01-02 16:24:33 -0800

    add partial files and updated to use them

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const path = require('path') // path is built in
 const express = require('express') // load via npm
+const hbs = require('hbs')
 
 const app = express()
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs') // need to load hbs via npm
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to server
 app.use(express.static(publicDirectoryPath))
@@ -21,8 +24,22 @@ app.get('', (req, res) => {
   })
 })
 
+app.get('/2020', (req, res) => {
+  res.render('2020', {
+    title: '2020 Photos'
+  })
+})
+
 app.get('/photoList', (req, res) => {
-  res.render('photoList')
+  res.render('photoList', {
+    title: 'Photos'
+  })
+})
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404 page not found'
+  })
 })
 
 app.listen(3004, () => {
